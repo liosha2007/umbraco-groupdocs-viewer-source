37fc965415cb1b5bfdc358dbb8f95252ce2b0c54
Sergey Zhuravel <sergey.zhuravel@groupdocs.com>
2012-10-01 14:30:42 +0300

    Initial commit for Umbraco GroupDocs Viewer Plugin V1.0 (Source code)

---
+++ ClassLibrary1/EmbedViewer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Groupdocs.EmbedViewer
+{
+    public class EmbedViewer : System.Web.UI.UserControl
+    {
+        public String Guid { get; set; }
+        public int FrameWidth { get; set; }
+        public int FrameHeight { get; set; }
+    }
+}
---
+++ Web/Views/EmbedViewer.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="Groupdocs.EmbedViewer.EmbedViewer" %>
+
+<iframe src="http://apps.groupdocs.com/document-viewer/embed/<%= Guid %>" frameborder="0" width="<%= FrameWidth %>" height="<%= FrameHeight %>"></iframe>
